bb3a4e1387ff34dd523b54443bf0ee31fc34a217
nesacar <nebojsart1409@yahoo.com>
2018-01-31 15:45:51 +0100

    added homepage

--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,19 @@ import '../css/main.scss';
 import fontawesome from '@fortawesome/fontawesome';
 import faPlus from '@fortawesome/fontawesome-free-solid/faPlus';
 import faBars from '@fortawesome/fontawesome-free-solid/faBars';
+import faAngleRight from '@fortawesome/fontawesome-free-solid/faAngleRight';
+import faMapMarkerAlt from '@fortawesome/fontawesome-free-solid/faMapMarkerAlt';
+import faPhone from '@fortawesome/fontawesome-free-solid/faPhone';
+import faFacebookF from '@fortawesome/fontawesome-free-brands/faFacebookF';
+import faInstagram from '@fortawesome/fontawesome-free-brands/faInstagram';
+import faEnvelope from '@fortawesome/fontawesome-free-regular/faEnvelope';
 
 // Add the icon to the library so you can use it in your page
-fontawesome.library.add(faPlus, faBars);
+fontawesome.library.add(faPlus, faBars, faAngleRight, faFacebookF, faInstagram, faMapMarkerAlt, faPhone, faEnvelope);
 
 $(function () {
     $('.collapse').collapse('hide');
+
     $('#trigger').click(function(){
         $('#side-menu').toggleClass('active');
 
@@ -30,4 +37,6 @@ $(function () {
     }, function () {
         $(this).find('ul').removeClass('open');
     });
+
+
 });
